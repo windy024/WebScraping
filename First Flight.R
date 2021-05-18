d337031554f4048fc8f2fc57e133d2e09191e092
Windy024 <aravind.be.mechengg@gmail.com>
2021-05-18 20:17:51 +0530

    Added a few comments

--- First Flight.R
+++ First Flight.R
@@ -1,16 +1,12 @@
 library(remotes)
 remotes::install_github('ewenme/understatr')
 library(understatr)
-View(get_leagues_meta())
-ars_1920 <- get_team_players_stats(team_name = "Arsenal", year = 2019)
-auba <- get_player_seasons_stats(player_id = 318)
-get_team_meta(team_name = "ARsenal")
 
 #1.get_league_seasons()
 get_league_seasons(league_name = "EPL")
-
+  
 #2.get_league_teams_stats()
-epl_1920 <- get_league_teams_stats(league_name = "EPL",year = 2019)
+epl_19 <- get_league_teams_stats(league_name = "EPL",year = 2019)
 
 #3.get_match_shots() - How to get Match ID? Available in 5
 chears <- get_match_shots(match_id = 14790)
@@ -18,7 +14,7 @@ chears <- get_match_shots(match_id = 14790)
 #4.get_match_stats() - How to get Match ID? Available in 5
 chears <- get_match_stats(match_id = 14790)
 
-#5.get_player_matches_stats()
+#5.get_player_matches_stats() - How to get player ID? Available in 9
 auba <- get_player_matches_stats(player_id = 318)
 
 #6.get_player_seasons_stats()
